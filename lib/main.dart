6ac3736a773b45a3a2efa9c8414cb1047ad506c5
Renzo R. Carpio Ponce <me@renzocarpio.com>
2019-05-26 11:42:08 -0500

    creando un home temporal

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:componentes/src/home_tem.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Componentes'),
+        ),
+        body: HomePageTemp()
+      ),
+    );
+  }
+}
---
+++ lib/src/home_tem.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Temp'),
+}
