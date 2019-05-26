58c4ea24bbd8aeadd1b06fb732a9e1a9e415eb34
Renzo R. Carpio Ponce <me@renzocarpio.com>
2019-05-26 12:05:55 -0500

    hometemp se utilizo listview, listtile y divider

--- lib/src/home_tem.dart
+++ lib/src/home_tem.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+        
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title'),
+          ),            
+        ],  
+      )
+    )
 }
